e259fa97bf059fb6124cfff639933604906bb0f5
Dương Thanh Lịch <135508140+thanhlichqnuu@users.noreply.github.com>
2024-06-09 10:02:13 +0700

    Add layout Nha_xe

---
+++ MotorRental.Consuming/wwwroot/lib_layout/js/deznav-init.js
@@ -0,0 +1,124 @@
+"use strict"
+
+var dezSettingsOptions = {};
+
+function getUrlParams(dParam) 
+	{
+		var dPageURL = window.location.search.substring(1),
+			dURLVariables = dPageURL.split('&'),
+			dParameterName,
+			i;
+
+		for (i = 0; i < dURLVariables.length; i++) {
+			dParameterName = dURLVariables[i].split('=');
+
+			if (dParameterName[0] === dParam) {
+				return dParameterName[1] === undefined ? true : decodeURIComponent(dParameterName[1]);
+			}
+		}
+	}
+
+/* Cookies Function */
+function setCookie(cname, cvalue, exhours) 
+	{
+		var d = new Date();
+		d.setTime(d.getTime() + (30*60*1000)); /* 30 Minutes */
+		var expires = "expires="+ d.toString();
+		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	}
+
+function getCookie(cname) 
+	{
+		/* var name = cname + "=";
+		var decodedCookie = decodeURIComponent(document.cookie);
+		var ca = decodedCookie.split(';');
+		for(var i = 0; i <ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return ""; */
+		
+		//new
+		var dc = document.cookie;
+		var prefix = cname + "=";
+		var begin = dc.indexOf("; " + prefix);
+		if (begin == -1) {
+			begin = dc.indexOf(prefix);
+			if (begin != 0) return null;
+		}
+		else
+		{
+			begin += 2;
+			var end = document.cookie.indexOf(";", begin);
+			if (end == -1) {
+			end = dc.length;
+			}
+		}
+		return decodeURI(dc.substring(begin + prefix.length, end));
+	}
+
+function deleteCookie(cname) 
+	{
+		var d = new Date();
+		d.setTime(d.getTime() + (1)); // 1/1000 second
+		var expires = "expires="+ d.toString();
+		//document.cookie = cname + "=1;" + expires + ";path=/";
+		document.cookie = cname + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT"+";path=/";
+	}
+
+function deleteAllCookie(reload = true)
+	{
+		jQuery.each(themeOptionArr, function(optionKey, optionValue) {
+				deleteCookie(optionKey);
+		});
+		if(reload){
+			location.reload();
+		}
+	}
+ 	
+/* Cookies Function END */	
+
+(function($) {
+	
+	"use strict"
+	
+	var direction =  getUrlParams('dir');
+	
+	dezSettingsOptions = {
+		typography: "poppins",
+		version: "light",
+		layout: "vertical",
+		headerBg: "color_1",
+		navheaderBg: "color_1",
+		sidebarBg: "color_1",
+		sidebarStyle: "full",
+		sidebarPosition: "fixed",
+		headerPosition: "fixed",
+		containerLayout: "full",
+		direction: direction
+	};
+
+	
+	if(direction == 'rtl')
+	{
+        direction = 'rtl'; 
+    }else{
+        direction = 'ltr'; 
+    }
+	
+	new dezSettings(dezSettingsOptions); 
+
+	jQuery(window).on('resize',function(){
+        /*Check container layout on resize */
+        dezSettingsOptions.containerLayout = $('#container_layout').val();
+        /*Check container layout on resize END */
+        
+		new dezSettings(dezSettingsOptions); 
+	});
+	
+})(jQuery);
